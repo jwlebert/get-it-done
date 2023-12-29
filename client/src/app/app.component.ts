b47309524dec4a38502458f463e34f535f7cbdc7
jwlebert <j.lebert4402@gmail.com>
2023-12-28 20:32:03 -0500

    added todo component and made it display

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,10 +1,14 @@
 import { Component } from '@angular/core';
+import { TodoComponent } from './todo/todo.component';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   title = 'client';
+  imports = [
+    TodoComponent
+  ];
 }
