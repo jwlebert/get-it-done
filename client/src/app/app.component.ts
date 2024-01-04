0643d8ed6423e99ac4895a5b51cb3f157ed0af69
jwlebert <j.lebert4402@gmail.com>
2024-01-04 15:42:21 -0500

    added ability to change themes

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,13 +1,38 @@
 import { Component, inject } from '@angular/core';
 import { TodoService } from './todo.service';
 
+const themes = {
+  'dark': {
+    'primary': "#121420",
+    'secondary': "#2C2B3C",
+    'contrast': "#FFFAFA",
+  },
+  'light': {
+    'primary': "#FFFFFF",
+    'secondary': "grey", // TODO
+    'contrast': "#000000",
+  }
+}
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
 })
+
 export class AppComponent {
   title = 'getitdone';
 
   todoService: TodoService = inject(TodoService);
+
+  currentTheme: number = 0;
+  changeTheme(): void {
+    this.currentTheme = (this.currentTheme + 1) % 2;
+
+    const theme = Object.values(themes)[this.currentTheme];
+
+    for (let [variable, value] of Object.entries(theme)) {
+      document.documentElement.style.setProperty(`--${variable}`, value);
+    }
+  }
 }
