418b4d7156ef1a81a870293dab672c1c02e4dc80
jwlebert <j.lebert4402@gmail.com>
2024-01-04 21:03:40 -0500

    hovering over the theme icon fills it

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,19 +1,6 @@
 import { Component, inject } from '@angular/core';
 import { TodoService } from './todo.service';
 
-const themes = {
-  'dark': {
-    'primary': "#121420",
-    'secondary': "#2C2B3C",
-    'contrast': "#FFFAFA",
-  },
-  'light': {
-    'primary': "#FFFFFF",
-    'secondary': "grey", // TODO
-    'contrast': "#000000",
-  }
-}
-
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -22,16 +9,34 @@ const themes = {
 
 export class AppComponent {
   title = 'getitdone';
-
   todoService: TodoService = inject(TodoService);
 
-  currentTheme: number = 0;
+  currentTheme: 'dark' | 'light' = 'dark';
+  themes = {
+    'hovered': '-outline',
+    'dark': {
+      'icon': 'moon',
+      'colors': {
+        'primary': "#121420",
+        'secondary': "#2C2B3C",
+        'contrast': "#FFFAFA",
+      }
+    },
+    'light': {
+      'icon': 'sunny',
+      'colors': {
+        'primary': "#FFFFFF",
+        'secondary': "#D3D3D3", // lightgrey < vscode color
+        'contrast': "#000000",
+      } 
+    }
+  }
+
   changeTheme(): void {
-    this.currentTheme = (this.currentTheme + 1) % 2;
+    this.currentTheme = (this.currentTheme === 'dark') ? 'light' : 'dark';
+    const theme = this.themes[this.currentTheme];
 
-    const theme = Object.values(themes)[this.currentTheme];
-
-    for (let [variable, value] of Object.entries(theme)) {
+    for (let [variable, value] of Object.entries(theme.colors)) {
       document.documentElement.style.setProperty(`--${variable}`, value);
     }
   }
